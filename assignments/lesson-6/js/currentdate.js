bbccbcc02199a45aae3636096df12b114439ef6a
Ash-Palmer <ashley.stevens333@yahoo.com>
2019-06-01 17:55:42 -0500

    added java to html

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,13 +1,5 @@
-var today= new Date();
-var weekday= new Array(7);
-weekday[0]= "Sunday";
-weekday[1]= "Monday";
-weekday[2]= "Tuesday";
-weekday[3]= "Wednesday";
-weekday[4]= "Thursday";
-weekday[5]= "Friday";
-weekday[6]= "Saturday";
-var n = weekday[today.getDay()];
+
+
 
 var dd= today.getDate();
 document.getElementById("currentdate").innerHTML=dd.getDate();
