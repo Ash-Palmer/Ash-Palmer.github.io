97f8f7a0e67a53bc6452c261bc4550c75abd77a9
Ash-Palmer <ashley.stevens333@yahoo.com>
2019-06-01 18:10:22 -0500

    added java

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,16 +1,7 @@
 
 
 
-var dd= today.getDate();
-document.getElementById("currentdate").innerHTML=dd.getDate();
 
-var mm= new Date();
-var months = ["January","February","March","April","May","June","July","August",
-"September","October","November","December"];
-document.getElementById("currentdate").innerHTML=months[mm.getMonth()];
-
-var yyyy= today.getFullYear();
-document.getElementById("currentdate").innerHTML=today.getFullYear();
 
 
 
