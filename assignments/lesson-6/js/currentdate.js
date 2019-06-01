ef428efd5ce00a812d911f890bd7b9c8b1e6ab5c
Ash-Palmer <ashley.stevens333@yahoo.com>
2019-06-01 18:19:02 -0500

    created hamburger js

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,4 +1,26 @@
-
+function myFunction() {
+    var today= new Date();
+    var weekday= new Array(7);
+    weekday[0]= "Sunday";
+    weekday[1]= "Monday";
+    weekday[2]= "Tuesday";
+    weekday[3]= "Wednesday";
+    weekday[4]= "Thursday";
+    weekday[5]= "Friday";
+    weekday[6]= "Saturday";
+    var n = weekday[today.getDay()];
+    document.getElementById("currentdate").innerHTML = n;
+    
+    var dd= today.getDate();
+    document.getElementById("currentdate").innerHTML=dd.getDate();
+    
+    var mm= new Date();
+    var months = ["January","February","March","April","May","June","July","August",
+    "September","October","November","December"];
+    document.getElementById("currentdate").innerHTML=months[mm.getMonth()];
+    
+    var yyyy= today.getFullYear();
+    document.getElementById("currentdate").innerHTML=today.getFullYear();}
 
 
 
---
+++ assignments/lesson-6/js/hamburger.js
@@ -0,0 +1,3 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classlist.toggle("goaway");
+}
