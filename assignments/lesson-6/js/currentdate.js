8512a02cf771ac7f9b152aa96de4992b5b11e279
Ash-Palmer <ashley.stevens333@yahoo.com>
2019-06-03 16:22:20 -0500

    java date

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,28 +1,16 @@
-function myFunction() {
-    var today= new Date();
-    var weekday= new Array(7);
-    weekday[0]= "Sunday";
-    weekday[1]= "Monday";
-    weekday[2]= "Tuesday";
-    weekday[3]= "Wednesday";
-    weekday[4]= "Thursday";
-    weekday[5]= "Friday";
-    weekday[6]= "Saturday";
-    var n = weekday[today.getDay()];
-    document.getElementById("currentdate").innerHTML = n;
-    
-    var dd= today.getDate();
-    document.getElementById("currentdate").innerHTML=dd.getDate();
-    
-    var mm= new Date();
-    var months = ["January","February","March","April","May","June","July","August",
-    "September","October","November","December"];
-    document.getElementById("currentdate").innerHTML=months[mm.getMonth()];
-    
-    var yyyy= today.getFullYear();
-    document.getElementById("currentdate").innerHTML=today.getFullYear();}
-
-
+function displayDate() {
+        var monthNames = [ "January", "February", "March", "April", "May", "June", 
+        "July", "August", "September", "October", "November", "December" ];
+        var dayNames = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+        "Friday", "Saturday" ];
+                
+        var newDate = new Date();
+                
+        var formattedDate = dayNames[newDate.getDay()] + ',' +' ' + newDate.getDate()
+         + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear();
+                
+        document.getElementById("outputdiv").innerHTML = formattedDate;
+    }
 
 
 
