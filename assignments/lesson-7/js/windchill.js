011dd282ef9cf656e6bf7f5d13ca06270689b584
Ash-Palmer <ashley.stevens333@yahoo.com>
2019-06-06 14:17:02 -0500

    added to fixed for no decimals

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -4,7 +4,7 @@ let s = parseInt(document.getElementById("wind speed").innerHTML);
 
 let result = 35.74 + (0.6215*t) - (35.75 * (s * 0.16)) + (0.4275 * t * (s*0.16));
 
-document.getElementById("output").innerHTML = result;
+document.getElementById("output").innerHTML = result.toFixed(0);
 
 
 
