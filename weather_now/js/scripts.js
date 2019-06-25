d729770998753c98f813843c4feaa73f265db6f9
Ash-Palmer <ashley.stevens333@yahoo.com>
2019-06-25 14:54:55 -0500

    temp added

--- weather_now/js/scripts.js
+++ weather_now/js/scripts.js
@@ -1,8 +1,10 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=72756,us&appid=e85ed115cdda94a41d0669084645767e", true );
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=72756,us&appid=e85ed115cdda94a41d0669084645767e&units=imperial", true );
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
-    
+
+    document.getElementById("place").innerHTML = weatherInfo.name;
+    document.getElementById("currentTemp").innerHTML = weatherInfo.main.temp;
 }
