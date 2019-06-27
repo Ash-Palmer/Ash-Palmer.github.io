ea146f7bed2f96b5522b91516cdf0a8a6734e35a
Ash-Palmer <ashley.stevens333@yahoo.com>
2019-06-27 15:05:07 -0500

    added https:

--- assignments/lesson-10/js/forecast.js
+++ assignments/lesson-10/js/forecast.js
@@ -1,5 +1,5 @@
 const weatherOb = new XMLHttpRequest();
-weatherOb.open("GET","//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e85ed115cdda94a41d0669084645767e&units=imperial", true);
+weatherOb.open("GET","https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e85ed115cdda94a41d0669084645767e&units=imperial", true);
 weatherOb.send();
 weatherOb.onload = function() {
     let weatherData = JSON.parse(weatherOb.responseText);
@@ -7,7 +7,7 @@ weatherOb.onload = function() {
 
     document.getElementById("currentTemp").innerHTML = weatherData.main.temp;
 
-    let icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"; 
+    let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"; 
     let desc = weatherData.weather[0].description;
 
     document.getElementById("weather_icon").setAttribute("src", icon);
