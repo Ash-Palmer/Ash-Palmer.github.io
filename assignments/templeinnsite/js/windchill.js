dbc4688fe16c1f9bc590fbf14598c818dfe57a08
Ash-Palmer <ashley.stevens333@yahoo.com>
2019-07-19 17:34:02 -0500

    updates

--- assignments/templeinnsite/js/windchill.js
+++ assignments/templeinnsite/js/windchill.js
@@ -1,6 +1,6 @@
 let t = parseInt(document.getElementById("maxTemp").innerHTML);
 
-let s = parseInt(document.getElementById("wind speed").innerHTML);
+let s = parseInt(document.getElementById("windspeed").innerHTML);
 
 let result = 35.74 + (0.6215*t) - (35.75 * (s * 0.16)) + (0.4275 * t * (s*0.16));
 
